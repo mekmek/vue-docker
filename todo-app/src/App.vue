ef8034336cb2051f578d24a34baf066211c6f409
kawasaki <k.kawasaki0820@gmail.com>
2021-03-28 17:06:52 +0900

    docker-compose.ymlでToDoコンテナを起動できるように実装

---
+++ todo-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ToDo />
+  </div>
+</template>
+
+<script>
+import ToDo from './components/ToDo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDo
+  }
+}
+</script>
+
+<style>
+</style>
